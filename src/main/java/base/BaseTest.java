b4d6b0c15533d359413818dc5d9ad182de8ba2a6
Aditya Singh <addie4120@gmail.com>
2026-04-25 13:06:29 +0530

    Refactor BaseTest: fix resource handling, move logger to class level, and improve setup stability

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -3,49 +3,40 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.Listeners;
 
-@Listeners(listeners.TestListener.class)
 public class BaseTest {
     protected Properties prop;
-    protected Logger logger;
+    protected final Logger logger = LogManager.getLogger(this.getClass());
 
     public Properties initializeProperties() {
         prop = new Properties();
-
-        try {
-            FileInputStream fis = new FileInputStream(
-                    System.getProperty("user.dir") + "/src/test/resources/config/config.properties"
-            );
+        try (FileInputStream fis = new FileInputStream(
+                System.getProperty("user.dir") + "/src/test/resources/config/config.properties"
+        )){
             prop.load(fis);
         } catch (IOException e) {
             throw new RuntimeException("Failed to load config.properties", e);
         }
-
         return prop;
     }
 
     @BeforeMethod(alwaysRun = true)
     public void setup() {
         prop = initializeProperties();
-
         DriverFactory.initDriver(prop);
-
-        logger = LogManager.getLogger(this.getClass());
+        DriverFactory.getDriver().get(prop.getProperty("baseUrl"));
         logger.info("Browser launched and URL opened");
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         DriverFactory.quitDriver();
-        logger.info("Browser closed");
+        logger.info("Browser closed for thread: " + Thread.currentThread().getId());
     }
 
     public WebDriver getDriver() {
