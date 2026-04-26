c42406db0cc84e692d841ee21cb5e34102dfda6a
Aditya Singh <addie4120@gmail.com>
2026-04-26 18:57:00 +0530

    test(wishlist): refactor TC017 to use proper TestNG annotations
    - Moved login and wishlist clearing logic to @BeforeMethod.
    - Isolated test actions from setup pre-conditions to reduce flakiness.
    - Cleaned up redundant log markers and improved assertion clarity.

--- src/test/java/tests/wishlist/TC017_AddProductToWishlistTest.java
+++ src/test/java/tests/wishlist/TC017_AddProductToWishlistTest.java
@@ -2,48 +2,45 @@ package tests.wishlist;
 
 import base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.*;
 import utils.TestData;
 
 public class TC017_AddProductToWishlistTest extends BaseTest {
+    private HomePage homePage;
+    private ProductPage productPage;
 
-    @Test(description = "Verify user can add product to wishlist and it appears in wishlist page")
-    public void verifyAddProductToWishlist() {
-
-        logger.info("Starting TC017: Add Product To Wishlist Test");
-
-        HomePage homePage = new HomePage(getDriver());
+    @BeforeMethod
+    public void setupWishlistState() {
+        homePage = new HomePage(getDriver());
         LoginPage loginPage = homePage.navigateToLogin();
         loginPage.login(TestData.EXISTING_EMAIL03, TestData.PASSWORD);
 
-        logger.info("Clearing wishlist before test");
-        WishlistPage wishlist = loginPage.navigateToWishlist();
+        logger.info("Setup: Clearing wishlist for " + TestData.EXISTING_EMAIL03);
+        WishlistPage wishlist = homePage.navigateToWishlist();
         wishlist.clearWishlist();
+    }
 
-        homePage = new HomePage(getDriver());
-
+    @Test(description = "Verify user can add product to wishlist successfully")
+    public void verifyAddProductToWishlist() {
+        // Test starts directly with the action
         SearchPage searchPage = homePage.searchProduct(TestData.PRODUCT_NAME);
-        ProductPage productPage = searchPage.openProduct();
+        productPage = searchPage.openProduct();
         String expectedProductName = productPage.getProductTitle();
 
-        logger.info("Adding product to wishlist");
+        logger.info("Adding product to wishlist: " + expectedProductName);
         productPage.addToWishlist();
-        String successMsg = productPage.getSuccessMessage();
 
         Assert.assertTrue(
-                successMsg.toLowerCase().contains("wish list"),
-                "Product was not added to wishlist successfully!"
+                productPage.getSuccessMessage().toLowerCase().contains("wish list"),
+                "Success message for wishlist addition not displayed!"
         );
 
-        logger.info("Navigating to wishlist page");
         WishlistPage wishlistPage = homePage.navigateToWishlist();
-
         Assert.assertTrue(
                 wishlistPage.isProductPresent(expectedProductName),
-                "Expected product is not present in wishlist!"
+                "Product '" + expectedProductName + "' was not found in the wishlist page!"
         );
-
-        logger.info("TC017 Passed");
     }
 }
